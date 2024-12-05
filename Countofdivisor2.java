016a873ee5fd633ccb066ff4abb980c7571567f5
waseem817 <s.shoaibwaseem786@gmail.com>
2024-12-05 14:48:46 +0530

    Add files via upload

---
+++ Countofdivisor2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Countofdivisor2
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new  Scanner(System.in);
+        System.out.println("enter number of test case:");
+        int t=sc.nextInt();
+        for(int i=0; i<t; i++)
+        {
+            int n=sc.nextInt();
+            int c=0;
+            for(int j=1; j*j<=n; j++)
+            {
+                
+                if((n%j)==0)
+                {
+                    c+=1;
+                    if(j*j!=n)
+                    {
+                        c+=1;
+                    }
+                }
+                
+            }
+            System.out.println(c);
+        }
+    }
+}
